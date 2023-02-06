fa88676b865301a424a00c1df91060f423885806
cepehao <cepeh@DESKTOP-N3BI952>
2023-02-06 15:46:56 +0500

    Пустой проект

---
+++ src/main/kotlin/ru/psu/eat/servicemodelling/ServiceModellingApplication.kt
@@ -0,0 +1,11 @@
+package ru.psu.eat.servicemodelling
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceModellingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceModellingApplication>(*args)
+}
